0e9931ac0b95b0fa0a359373e759aa938b9fc195
Oreder <stepup.ored@gmail.com>
2018-09-15 21:35:44 +0300

    Updating requirements and making tests for integration

--- django/app/tests.py
+++ django/app/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+
+class ViewTest(TestCase):
+	def testAppView(self):
+		resp = self.client.get('/app/')
+		self.assertEqual(resp.status_code, 200)
+		
+	def testAppProfile(self):
+		resp = self.client.get('/app/profile/')
+		self.assertEqual(resp.status_code, 200)
+	
+	def testProfile(self):
+		resp = self.client.post('/app/profile/', {'user': 'mike'} )
+		self.assertEqual(resp.status_code, 200)
+
+	def testProfileContent(self):
+		resp = self.client.post('/app/profile/', {'user': 'mike'} )
+		self.assertNotEqual(resp.content, '')
--- django/demonstration/settings.py
+++ django/demonstration/settings.py
@@ -15,8 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -39,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'app',
+	'django_nose'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
